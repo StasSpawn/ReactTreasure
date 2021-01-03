6ed828865ac0b2cedfe2a4e6b3debe31516f774a
Stas <s.moshchenskyi@altoros.com>
2021-01-03 16:16:33 +0300

    Lesson 16 (Firebase Gmail Nodemailer)

---
+++ functions/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  email: 'spawn200995@gmail.com',
+  password: 'power123'
+}
--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,39 @@
 const functions = require('firebase-functions');
+const nodemailer = require('nodemailer')
+const htmlToText = require('nodemailer-html-to-text').htmlToText;
+const { email, password } = require('./config');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const transporter = nodemailer.createTransport ({
+  service: 'gmail',
+  auth: {
+    user: email,
+    pass: password
+  }
+})
+
+transporter.use('compile', htmlToText());
+
+const sendOrderEmail = data => {
+  const options = {
+    from: `MrDonald's <${email}>`,
+    to: data.email,
+    subject: 'Вам заказ',
+    html: `
+      <div>
+      <h2>Добрый день ${data.nameClient}</h2>
+      <h3>Ваш заказ:</h3>
+      <ul>
+        ${data.order.map(({itemName, count, price }) => (
+          `<li>${itemName} - ${count} ${price * count} руб.</li>`
+        ))}
+        <p>Итого ${data.order.reduce((sum, item) => sum + (item.price + item.count), 0)}</p>
+      </ul>  
+             
+      </div>
+    `
+  }
+
+  transporter.sendMail(options)
+}
+
+exports.sendUserEMail = functions.database.ref('orders/{pushID}').onCreate(order => console.log(order.val()));
